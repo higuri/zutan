dcba8651c6089efcbbe56c6ff892de243289aa77
higuri <higuri36@gmail.com>
2019-01-07 21:20:16 +0900

    Change style with Grid

--- src/SearchResult.tsx
+++ src/SearchResult.tsx
@@ -3,65 +3,68 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 import Fab from '@material-ui/core/Fab';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import withWidth from '@material-ui/core/withWidth';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import SearchResultRow from './SearchResultRow';
 
-// SearchResultDiv 
-const SearchResultDiv = styled.div`
-  margin-top: 20px;
-  margin-left: 10px;
-  margin-right: 10px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: start;
-  align-items: flex-start;
-` as any;
-// ImageCard
-const ImageCard = styled(Card)`
-  && {
-    margin: 5px;
-  }
-` as any;
-// ResultImg
-const ResultImg = styled.img`
-  width: 100px;
-  object-fit: contain;
+// ResultDiv
+const ResultDiv = styled.div`
+  margin: 30px 0;
 `;
+// MoreResultsDiv
 const MoreResultsDiv = styled.div`
   margin: 10px auto;
   display: flex;
   justify-content: center;
 `;
 
-/// SearchResultProps
+// SearchResultProps
 interface SearchResultProps {
+  width: string;
   imageURLs: string[];
   onImageClicked: (iImages: number) => void;
   onMoreResultsClicked: () => void;
 }
 
-/// SearchResult
+// SearchResult
 class SearchResult extends Component<SearchResultProps> {
 
   // render()
   render() {
+    const width = this.props.width;
+    let unit;
+    switch (width) {
+      case 'xs':
+        unit = 2;
+        break;
+      case 'sm':
+        unit = 3;
+        break;
+      default:
+        unit = 4;
+        break;
+    }
+    let rows: string[][] = [];
+    this.props.imageURLs.forEach((url, i) => {
+      if (i % unit === 0) {
+        rows.push([]);
+      }
+      rows[rows.length - 1].push(url);
+    });
+    //
     return (
-      <div>
-        <SearchResultDiv>
-          {
-            this.props.imageURLs.map((url, i) => (
-              <ImageCard
-                raised
-                key={i}
-                onClick={() => this.props.onImageClicked(i)}>
-                <CardContent>
-                  <ResultImg src={url} />
-                </CardContent>
-              </ImageCard>
-            ))
-          }
-        </SearchResultDiv>
+      <ResultDiv>
+        {
+          rows.map((urls, i) => (
+            <SearchResultRow
+              key={i}
+              imageURLs={urls}
+              onImageClicked={(j) => {
+                this.props.onImageClicked(i*unit + j);
+              }}>
+            </SearchResultRow>
+          ))
+        }
         {
           (0 < this.props.imageURLs.length) && (
             <MoreResultsDiv>
@@ -74,9 +77,9 @@ class SearchResult extends Component<SearchResultProps> {
             </MoreResultsDiv>
           )
         }
-      </div>
+      </ResultDiv>
     );
   }
 }
 
-export default SearchResult;
+export default withWidth()(SearchResult);
---
+++ src/SearchResultRow.tsx
@@ -0,0 +1,58 @@
+// SearchResultRow.tsx
+
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+// RowDiv
+const RowDiv = styled.div`
+  margin: 10px 0;
+`;
+
+// ResultImg
+const ResultImg = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+`;
+
+// SearchResultRowProps
+interface SearchResultRowProps {
+  imageURLs: string[];
+  onImageClicked: (iImages: number) => void;
+}
+
+// SearchResultRow
+class SearchResultRow extends Component<SearchResultRowProps> {
+
+  // render()
+  render() {
+    return (
+      <RowDiv>
+        <Grid
+          container
+          spacing={8}>
+        {
+          this.props.imageURLs.map((url, i) => (
+            <Grid
+              key={i}
+              item xs>
+              <Card
+                raised
+                onClick={() => this.props.onImageClicked(i)}>
+                <CardContent>
+                  <ResultImg src={url} />
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        }
+        </Grid>
+      </RowDiv>
+    );
+  }
+}
+
+export default SearchResultRow;
